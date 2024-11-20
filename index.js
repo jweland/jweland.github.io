469b4995f3262db70c391c7e71de5e167c169154
jweland <jillianweland@outlook.com>
2024-11-20 13:40:56 -0600

    lots of updates on pages

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
-
+document.addEventListener("DOMContentLoaded", () => {
+    const navLinks = document.querySelectorAll(".navbar a");
+    const currentPage = window.location.pathname.split("/").pop();
+  
+    navLinks.forEach(link => {
+      if (link.getAttribute("href") === currentPage) {
+        link.classList.add("active"); 
+      }
+    });
+  });
